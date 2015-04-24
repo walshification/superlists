a0f58193fd0e3ca131393baed6b8bc0933cb7f79
Christopher Walsh <walshification@gmail.com>
2015-04-23 21:34:24 -0500

    Fixed typos in FT and front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -19,12 +19,13 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_tag_name('h1').text
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-                inputbox.get_attributes('placeholder'),
+                inputbox.get_attribute('placeholder'),
                 'Enter a to-do item'
         )
 
@@ -39,7 +40,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.test == '1: Buy peacock feathers' for row in rows)
+            any(row.test == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         # There is still a text box inviting her to add another item. She
