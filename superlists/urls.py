5c9600f42b61a40942079cf2818781601f9c49f1
Christopher Walsh <walshification@gmail.com>
2015-06-16 21:18:16 -0500

    pep8ified

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+# from django.contrib import admin
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^$', 'lists.views.home_page', name='home'),
     url(r'^lists/', include('lists.urls')),
     # url(r'^admin/', include(admin.site.urls)),
