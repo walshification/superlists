45997b88ecf2ebb57c2f6db8d1ca51cc16a67fa8
Christopher Walsh <walshification@gmail.com>
2019-11-10 13:01:41 -0600

    Use latest driver and make it a test

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,19 @@
+import unittest
+
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
 
-assert "Django" in browser.title
+class BrowserTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def test_browser_can_navigate_to_site(self):
+        self.browser.get("http://localhost:8000")
+        assert "Django" in self.browser.title
+
+    def tearDown(self):
+        self.browser.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
