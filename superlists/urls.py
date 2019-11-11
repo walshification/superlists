aaf29a8a1f9045aaac918a4e86da9870382aef45
Christopher Walsh <walshification@gmail.com>
2019-11-11 13:44:16 -0600

    Remove hardcoded urls

--- superlists/urls.py
+++ superlists/urls.py
@@ -6,7 +6,7 @@ from lists import views
 
 
 urlpatterns = [
-    url(r"^$", views.home_page, name="lists/home.html"),
+    url(r"^$", views.home_page, name="homepage"),
     url(r"^lists/", include("lists.urls")),
     # url(r'^admin/', include(admin.site.urls)),
 ]
