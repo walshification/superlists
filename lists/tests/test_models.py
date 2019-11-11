cb25eff3d596c6e86e36cfb1fbdcdd66d56e641e
Christopher Walsh <walshification@gmail.com>
2019-11-11 13:28:06 -0600

    Use f-strings instead of formatters

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -41,4 +41,4 @@ class ListAndItemModelsTest(TestCase):
 
     def test_get_absolute_url(self):
         list_ = List.objects.create()
-        self.assertEqual(list_.get_absolute_url(), "/lists/%d/" % (list_.id,))
+        self.assertEqual(list_.get_absolute_url(), f"/lists/{list_.id}/")
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -18,11 +18,11 @@ class HomePageTest(TestCase):
 class ListViewTest(TestCase):
     def post_invalid_input(self):
         list_ = List.objects.create()
-        return self.client.post("/lists/%d/" % (list_.id,), data={"text": ""})
+        return self.client.post(f"/lists/{list_.id}/", data={"text": ""})
 
     def test_uses_list_template(self):
         list_ = List.objects.create()
-        response = self.client.get("/lists/%d/" % (list_.id,))
+        response = self.client.get(f"/lists/{list_.id}/")
         self.assertTemplateUsed(response, "lists/list.html")
 
     def test_displays_only_items_for_that_list(self):
@@ -33,7 +33,7 @@ class ListViewTest(TestCase):
         Item.objects.create(text="other list item 1", list=other_list)
         Item.objects.create(text="other list item 2", list=other_list)
 
-        response = self.client.get("/lists/%d/" % (correct_list.id,))
+        response = self.client.get(f"/lists/{correct_list.id}/")
 
         self.assertContains(response, "itemey 1")
         self.assertContains(response, "itemey 2")
@@ -51,7 +51,7 @@ class ListViewTest(TestCase):
         correct_list = List.objects.create()
 
         self.client.post(
-            "/lists/%d/" % (correct_list.id,),
+            f"/lists/{correct_list.id}/",
             data={"text": "A new item for an existing list"},
         )
 
@@ -65,11 +65,11 @@ class ListViewTest(TestCase):
         correct_list = List.objects.create()
 
         response = self.client.post(
-            "/lists/%d/" % (correct_list.id,),
+            f"/lists/{correct_list.id}/",
             data={"text": "A new item for an existing list"},
         )
 
-        self.assertRedirects(response, "/lists/%d/" % (correct_list.id,))
+        self.assertRedirects(response, f"/lists/{correct_list.id}/")
 
     def test_invalid_input_nothing_saved_to_db(self):
         self.post_invalid_input()
@@ -90,7 +90,7 @@ class ListViewTest(TestCase):
 
     def test_displays_item_form(self):
         list_ = List.objects.create()
-        response = self.client.get("/lists/%d/" % (list_.id,))
+        response = self.client.get(f"/lists/{list_.id}/")
         self.assertIsInstance(response.context["form"], ItemForm)
         self.assertContains(response, 'name="text"')
 
@@ -106,7 +106,7 @@ class NewListTest(TestCase):
         response = self.client.post("/lists/new", data={"text": "A new list item"})
         new_list = List.objects.first()
         self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, "/lists/%d/" % (new_list.id,))
+        self.assertRedirects(response, f"/lists/{new_list.id}/")
 
     def test_validation_errors_are_sent_back_to_home_page_template(self):
         response = self.client.post("/lists/new", data={"text": ""})
